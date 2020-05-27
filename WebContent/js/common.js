fa356adda12200ecb148d8cae50da5f231237c29
Atit S Gaonkar <atit.sgaonkar@gmail.com>
2020-05-26 17:08:42 -0700

    Incorporated Login Funtcionality
    Incorporated Login Functionality. Handled success and failure.

--- WebContent/js/common.js
+++ WebContent/js/common.js
@@ -14,6 +14,21 @@ function sendAjaxRequest(url, successCallBackFn) {
 	});
 }
 
+function ValidateEmail(mail) {
+	console.log('Validate');
+	if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)) {
+		return (true)
+	}
+	// alert("You have entered an invalid email address!")
+	return (false)
+}
+function ValidateName(name) {
+	if (/^([a-zA-Z ]){1,15}$/.test(name)) {
+		return (true)
+	}
+	// alert("You have entered an invalid name!")
+	return (false)
+}
 
 function checkAndAlertError(message) {
 	if(message.includes("fail") || message.includes("error")) {
@@ -25,6 +40,16 @@ function makeURL(url, params) {
 	var paramsAsURL = url + "?";
 	var separator = ""
 	for(var key in params) {
+		paramsAsURL = paramsAsURL + separator + key + "=" + params[key];
+		separator = "&";
+	}
+	return paramsAsURL;
+}
+
+function makeLoginURL(url, params){
+	var paramsAsURL = url + "?";
+	var separator = ""
+	for (var key in params) {
 		paramsAsURL = paramsAsURL + separator + key + "=" + params[key];
 		separator = "&";
 	}

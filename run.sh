c4b822a961b96192d0dd303a9cd0e248b705f99e
akrish84 <akrish84@asu.edu>
2020-08-26 11:35:23 -0700

    1. Adding new jar file jaxb which is obsolete in java version 11. This causes runtime exception. 2. Updating run.sh to remove classes dir everytime while building project. 3. Removing empty space in struts.xml 4. Removing class files which were cached in git. 5. Adding source and target javac compiler to build.xml, else it causes Java Version exception while running in some cases.

--- run.sh
+++ run.sh
@@ -1,18 +1,46 @@
+CLASSES_DIR=build/classes
 TOMCAT_DIR=build/apache-tomcat-8.5.55
 TOMCAT_PATH=/Users/akhilesh/softwares/apache-tomcat-8.5.55
 TOMCAT_WEBAPPS=$TOMCAT_DIR/webapps
 WAR_FILE=build/war/ROOT.war
 
 
-ant -f build.xml
-exit_status=$?
-if [ $exit_status -ne 0 ]
-then
-	exit $exit_status
-fi
-rm -rf TOMCAT_DIR
-cp -r $TOMCAT_PATH ./build/
-rm -rf $TOMCAT_WEBAPPS/ROOT.war $TOMCAT_WEBAPPS/ROOT
-cp $WAR_FILE $TOMCAT_WEBAPPS
-sh $TOMCAT_DIR/bin/catalina.sh run
+clean_classes_dir() {
+	rm -rf $CLASSES_DIR
+	print_message "Creating Dir: $CLASSES_DIR"
+	mkdir -p $CLASSES_DIR
+}
+
+
+ant_build() {
+	print_message "Building Project: ant -f build.xml"
+	ant -f build.xml
+	exit_status=$?
+	if [ $exit_status -ne 0 ]
+	then
+        	exit $exit_status
+	fi
+	print_message "Project Build SUCCESSFUL"
+
+}
+
+run_server() {
+	rm -rf TOMCAT_DIR
+	cp -r $TOMCAT_PATH ./build/
+	rm -rf $TOMCAT_WEBAPPS/ROOT.war $TOMCAT_WEBAPPS/ROOT
+	cp $WAR_FILE $TOMCAT_WEBAPPS
+	print_message "STARTING TOMCAT SERVER"
+	sh $TOMCAT_DIR/bin/catalina.sh run
+}
+
+print_message() {
+	echo "--------------------------------------------------------------------------------------------"
+	echo "-------------$@----------------"
+	echo "--------------------------------------------------------------------------------------------"
+}
+
+clean_classes_dir
+ant_build
+run_server
+
 
